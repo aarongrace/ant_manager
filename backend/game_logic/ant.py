07d998ca4fc246cd2a7a0446aca70f2236d33f3c
Aaron Grace <aarongraceart@gmail.com>
2025-04-15 02:46:53 -0500

    fixed one more bug

--- backend/game_logic/ant.py
+++ backend/game_logic/ant.py
@@ -55,7 +55,7 @@ def initialize_guest_ants():
             ant_name="Queenie",
             type=TypeEnum.queen,
             task=TaskEnum.idle,
-            position={"x": 0.2, "y": 0.5},
+            position={"x": 0.9, "y": 0.7},
             age=2,
             target="",
             destination="",
