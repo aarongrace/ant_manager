0616e77cca283107455cd0aeead074687cb0525a
Aaron Grace <aarongraceart@gmail.com>
2025-04-28 20:41:14 -0500

    fixed cors

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.add_middleware(AuthMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
